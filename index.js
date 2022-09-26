f297284f0b8be02bc4c3fadca6dbb3683d9600d2
ana-lejava <ana.lejava@toptal.com>
2022-09-26 14:49:28 +0400

    Frames are moving!

--- index.js
+++ index.js
@@ -6,10 +6,39 @@ window.on = window.addEventListener;
 
 // 3D Slider implementation
 
+const frameDistance = 100;
+
+let scene, frames;
+
+setupInitialFramesPosition = () => {
+    frames.forEach((frame, frameIndex) => {
+        const startFramePosition = -(frameIndex * frameDistance);
+
+        frame.style.setProperty(
+            '--start-frame-position',
+            `${startFramePosition}px`
+        );
+    });
+}
+
 // Page onload
 window.on('load', () => {
     console.log('Page is loaded');
 
-    console.log($('body'));
-    console.log($$('.scene-frame'));
+    scene = $('.scene-3d');
+    frames = $$('.scene-frame');
+
+    setupInitialFramesPosition();
+})
+
+// Page onscroll
+window.on('scroll', () => {
+    const scrollPosition = document.documentElement.scrollTop;
+
+    frames.forEach((frame) => {
+        frame.style.setProperty(
+            '--scroll-position',
+            `${scrollPosition}px`
+        );
+    });
 })
